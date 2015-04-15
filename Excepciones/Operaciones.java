0e0df515d2c242d7733fe10b2e02160880d03324
Jimmy <jvillarreal337@gmail.com>
2015-04-14 21:20:28 -0500

    Removed files other than .java from the folder.

---
+++ Excepciones/Operaciones.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package package1;
+
+class Operaciones {
+    public static int division(int b, int c) throws ArithmeticException
+    {
+        return b/c;
+    }
+}
